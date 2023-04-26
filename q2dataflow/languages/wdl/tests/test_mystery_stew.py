2ed1e183372448e1eb8fe454259a8ae2ad427fef
Evan Bolyen <ebolyen@gmail.com>
2023-04-26 15:00:29 -0700

    Skip sleep in gh-actions

--- q2dataflow/languages/wdl/tests/test_mystery_stew.py
+++ q2dataflow/languages/wdl/tests/test_mystery_stew.py
@@ -37,10 +37,9 @@ def get_tests():
 
 @pytest.mark.parametrize('action,example', get_tests(), ids=_labeler)
 def test_mystery_stew(action, example, docker_image):
-    # TODO Discuss w Evan if better way to handle Docker Desktop crash due to
-    #  lots of rapid start-up/shut-down cycles; for now, just give
-    #  Docker Desktop some time to clear its mind between tests
-    time.sleep(2)
+    if os.environ.get('SKIP_SLEEP') is None:
+        # Prevent docker-desktop crash
+        time.sleep(1)
 
     example_f = action.examples[example]
     use = WdlTestUsage(docker_image=docker_image)
